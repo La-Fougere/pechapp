667d830bfdedb4a4b9518b4ef1d7b7e49f2c1d83
LaFougere <timothe.lair@lafougere.dev>
2026-03-31 18:46:01 +0200

    build.sh update to avoid 404

--- build.sh
+++ build.sh
@@ -3,3 +3,13 @@ npm run build
 sleep 5
 rm -rf ~/public_html/*
 mv ~/pechapp/app/dist/* ~/public_html/
+cat > ~/public_html/.htaccess <<'EOF'
+<IfModule mod_rewrite.c>
+  RewriteEngine On
+  RewriteBase /
+  RewriteRule ^index\.html$ - [L]
+  RewriteCond %{REQUEST_FILENAME} !-f
+  RewriteCond %{REQUEST_FILENAME} !-d
+  RewriteRule . /index.html [L]
+</IfModule>
+EOF
